cde44903a3632e1e855e694154175427c2c9eaf8
Iker De Loma-Osorio <ikdlo@vestas.com>
2025-08-18 13:31:43 +0200

    Rename resolution function to get_resolution as it was creating problems when defining it as a variable for its usage

--- src/signals.py
+++ src/signals.py
@@ -1,4 +1,4 @@
 from typing import Union
 
-def resolution(sampling_frequency: Union[int, float]) -> Union[int, float]:
+def get_resolution(sampling_frequency: Union[int, float]) -> Union[int, float]:
     return 1 / sampling_frequency
--- tests/src/test_signals.py
+++ tests/src/test_signals.py
@@ -1,10 +1,10 @@
 import unittest
-from src.signals import resolution
+from src.signals import get_resolution
 
 
 class TestSignals(unittest.TestCase):
-    def test_resolution(self) -> None:
+    def test_get_resolution(self) -> None:
         sampling_frequency = 100
         expected_result = 0.01
-        result = resolution(sampling_frequency)
+        result = get_resolution(sampling_frequency)
         self.assertEqual(result, expected_result)
