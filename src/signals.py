7f3f3a4ed2a0324d2586231396bf15e41b4f8f0a
Iker De Loma-Osorio <ikdlo@vestas.com>
2025-08-23 19:42:36 +0200

    Add docstrings to get_resolution function

--- src/signals.py
+++ src/signals.py
@@ -1,4 +1,10 @@
 from typing import Union
 
 def get_resolution(sampling_frequency: Union[int, float]) -> Union[int, float]:
+    """
+    Calculate the signal resolution in seconds from the sampling frequency.
+
+    :param sampling_frequency: sampling frequency in Hertz
+    :return: signal resolution in seconds
+    """
     return 1 / sampling_frequency
