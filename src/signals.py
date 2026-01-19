0b2af176d30c504ecc72ab81a5e5a7ede0139e7b
idloea <ikerde1993@gmail.com>
2026-01-19 16:03:50 +0000

    Add other calculations for the signal

--- src/signals.py
+++ src/signals.py
@@ -1,6 +1,7 @@
 from typing import Union
+import numpy as np
 
-def resolution(sampling_frequency: Union[int, float]) -> Union[int, float]:
+def calculate_resolution(sampling_frequency: Union[int, float]) -> Union[int, float]:
     """
     Calculate the signal resolution in seconds from the sampling frequency.
 
@@ -8,3 +9,57 @@ def resolution(sampling_frequency: Union[int, float]) -> Union[int, float]:
     :return: signal resolution in seconds
     """
     return 1 / sampling_frequency
+
+def calculate_root_mean_square(y: np.ndarray) -> float:
+    """
+    Calculate the root mean square (RMS) of a signal.
+
+    :param y: Array or iterable of signal values
+    :return: RMS value of the signal
+    """
+    return np.sqrt(np.mean(y**2))
+
+def calculate_absolute_peak(y: np.ndarray) -> float:
+    """
+    Calculate the peak (maximum absolute value) of a signal.
+
+    :param y: Array or iterable of signal values
+    :return: Peak value of the signal
+    """
+    return np.max(np.abs(y))
+
+def calculate_crest_factor(y: np.ndarray) -> float:
+    """
+    Calculates the crest factor of a signal.
+
+    :param y: Array or iterable of signal values
+    :return: Crest factor value of the signal
+    """
+    
+    peak = calculate_absolute_peak(y=y)
+    root_mean_square = calculate_root_mean_square(y=y)
+    return peak / root_mean_square
+
+def calculate_average_rectified_value(y: np.ndarray) -> float:
+    """
+    Calculates the Average Rectified Value (ARV) of a given input signal.
+    
+    :param y: Array or iterable of signal values
+    :return: Average Rectified Value (ARV) of the signal
+    """
+
+    return np.mean(np.abs(y))
+
+def calculate_shape_factor(y: np.ndarray) -> float:
+    """
+    Calculates the shape factor of a given input signal.
+    
+    Args:
+        signal (array-like): The input time-series signal.
+        
+    Returns:
+        float: The shape factor value.
+    """
+    rms = calculate_root_mean_square(y=y)
+    arv = calculate_average_rectified_value(y=y)
+    return rms / arv
--- tests/src/test_signals.py
+++ tests/src/test_signals.py
@@ -1,10 +1,49 @@
 import unittest
-from src.signals import resolution
+from src.signals import calculate_absolute_peak, calculate_average_rectified_value, calculate_crest_factor, calculate_resolution, calculate_root_mean_square, calculate_shape_factor
+import numpy as np
 
 
 class TestSignals(unittest.TestCase):
     def test_resolution(self) -> None:
         sampling_frequency = 100
         expected_result = 0.01
-        result = resolution(sampling_frequency)
+        result = calculate_resolution(sampling_frequency)
         self.assertEqual(result, expected_result)
+    
+    def test_root_mean_square(self) -> None:
+        signal = np.array([1, 2])
+        result = calculate_root_mean_square(signal)
+        expected_result = np.sqrt(2.5)        
+        self.assertEqual(result, expected_result)
+
+    def test_calculate_absolute_peak(self) -> None:
+        signal = np.array([-3, 1, 2, -4, 0])
+        result = calculate_absolute_peak(signal)
+        expected_result = 4
+        self.assertEqual(result, expected_result)
+
+    def test_calculate_absolute_peak_empty_array(self) -> None:
+        with self.assertRaises(ValueError):
+            calculate_absolute_peak(np.array([]))        
+    
+    def test_crest_factor(self) -> None:
+        signal = np.array([1, -2, 3, -4, 5])
+        result = calculate_crest_factor(signal)
+        expected_peak = 5
+        expected_rms = calculate_root_mean_square(y=signal)
+        expected_crest_factor = expected_peak / expected_rms
+        self.assertAlmostEqual(result, expected_crest_factor)
+
+    def test_calculate_average_rectified_value(self) -> None:
+        signal = np.array([-1, 2, -3, 4, -5])
+        result = calculate_average_rectified_value(signal)
+        expected_result = 3.0
+        self.assertEqual(result, expected_result)
+
+    def test_calculate_shape_factor(self) -> None:
+        signal = np.array([1, -2, 3, -4, 5])
+        result = calculate_shape_factor(signal)
+        expected_rms = calculate_root_mean_square(y=signal)
+        expected_arv = calculate_average_rectified_value(y=signal)
+        expected_shape_factor = expected_rms / expected_arv
+        self.assertAlmostEqual(result, expected_shape_factor)
